6477cd950b7d6341551a16e3c76cce571d68a1a1
Azzam Salim Al Zadjali <azzam323@hotmail.com>
2026-04-26 00:52:25 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from keep_alive import keep_alive
 # ==========================================
 # 🔴 التوكين الخاص بك
 # ==========================================
-TOKEN = "7976756950:AAEKuCwy2Y_PsqX6KMLZoJE4tORKl3F-uhM"
+TOKEN = "7976756950:AAGs4odFu9fABU0nYNUnuCUJyB4QIdINgS4"
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
